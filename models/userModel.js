31eb4275ed63c442a6f17685bb09c91d0ab18b25
Agam Jain <jainagam4@gmail.com>
2024-10-17 23:28:12 +0530

    Refactor URL model to include visit history

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose, { model } from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+    shortId: { type: String, required: true },
+    redirectUrl: { type: String, required: true },
+    visitHistory: [{
+        timestamp: { type: Number }
+    }]
+}, { timestamps: true });
+
+const Url = mongoose.model("url", urlSchema);
+model.exports = Url;
